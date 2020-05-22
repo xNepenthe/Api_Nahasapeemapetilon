c83cfeb42d96be9a2b2b9edbbab6abcd7432418b
xNepenthe <milanofernandomartin@gmail.com>
2020-05-22 02:31:10 -0300

    added middlewares

---
+++ src/middlewares/validateMiddleware.js
@@ -0,0 +1,24 @@
+const joi = require('@hapi/joi');
+
+const schemaBody = joi.object({
+    name: joi.string().alphanum().min(3).max(20).required().messages({
+        'string.base': 'El campo "Name" debe ser un texto.',
+        'string.alphanum': 'El campo "Name" debe tener solamente letras y numeros.',
+        'string.empty': 'El campo "Name" no puede estar vacio.'
+    }),
+    age: joi.number().required(),
+    email: joi.string().email().required(),
+})
+
+const schemaHeader = joi.object();
+
+async function validateRequest(req, res, next) { 
+    try {
+        await schemaBody.validateAsync(req.body);
+        next();
+    } catch (err) {
+        next(err)
+    }
+}
+
+module.exports = validateRequest;
